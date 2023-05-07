64b11a28ede0edff19ac92b4fe6923eef29bd67b
Marios Sofokleous <marios.sofokleous@yandex.com>
2023-05-07 21:35:14 +0300

    Enable strict mode. Add RecoilRoot.

--- src/index.js
+++ src/index.js
@@ -3,29 +3,38 @@ import Single from './routes/Single';
 import './index.scss';
 import { createRoot, render } from '@wordpress/element';
 import { HashRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from 'recoil';
 
 const domElement = document.getElementById('jlRoot');
 
 if (createRoot) {
   createRoot(domElement).render(
-    <HashRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Multi />} />
-          <Route path=":id" element={<Single />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <React.StrictMode>
+      <RecoilRoot>
+        <HashRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Multi />} />
+              <Route path=":id" element={<Single />} />
+            </Route>
+          </Routes>
+        </HashRouter>
+      </RecoilRoot>
+    </React.StrictMode>
   );
 } else {
   render(
-    <HashRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Multi />} />
-          <Route path=":id" element={<Single />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <React.StrictMode>
+      <RecoilRoot>
+        <HashRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Multi />} />
+              <Route path=":id" element={<Single />} />
+            </Route>
+          </Routes>
+        </HashRouter>
+      </RecoilRoot>
+    </React.StrictMode>
   );
 }
