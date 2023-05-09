5389cfe3041cfa0c841890f536702407963c9a2c
Marios Sofokleous <marios.sofokleous@yandex.com>
2023-05-09 23:23:10 +0300

    feat: get data from endpoints and write to recoil state.

--- src/routes/Multiple.js
+++ src/routes/Multiple.js
@@ -45,6 +45,55 @@ function Multiple() {
     process.env.APP_BASE_URL_PROD + process.env.APP_ENDPOINT_EXPERIENCE_LEVELS
   );
 
+  async function getData() {
+    try {
+      /**
+       * Fetch jobs and set recoil state
+       */
+      const response1 = await fetch(endpointJobs + '?per_page=' + process.env.APP_PER_PAGE);
+      const data1 = await response1.json();
+      setAllJobs(data1);
+
+      /**
+       * Fetch locations and set recoil state
+       */
+      const response2 = await fetch(endpointLocations + '?per_page=' + process.env.APP_PER_PAGE);
+      const data2 = await response2.json();
+      // Filter out locations with a count of 0
+      setAllLocations(data2.filter(location => location.count > 0));
+
+      /**
+       * Fetch categories and set recoil state
+       */
+      const response3 = await fetch(endpointCategories + '?per_page=' + process.env.APP_PER_PAGE);
+      const data3 = await response3.json();
+      // Filter out categories with a count of 0
+      setAllCategories(data3.filter(category => category.count > 0));
+
+      /**
+       * Fetch types and set recoil state
+       */
+      const response4 = await fetch(endpointTypes + '?per_page=' + process.env.APP_PER_PAGE);
+      const data4 = await response4.json();
+      // Filter out types with a count of 0
+      setAllTypes(data4.filter(type => type.count > 0));
+
+      /**
+       * Fetch experience levels and set recoil state
+       */
+      const response5 = await fetch(endpointExperienceLevels + '?per_page=' + process.env.APP_PER_PAGE);
+      const data5 = await response5.json();
+      // Filter out experience levels with a count of 0
+      setAllExperienceLevels(data5.filter(experienceLevel => experienceLevel.count > 0));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []); // An empty array means it only runs once (after the component is mounted).
+
   return (
     <div>Multiple</div>
   )
