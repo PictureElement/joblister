6b81ddca81edb11fe1e867c201974662c596397f
Marios Sofokleous <marios.sofokleous@yandex.com>
2023-05-13 17:41:21 +0300

    Added _settings file to store sass variables. Made markup and styling changes for the JobItem component.

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Examples:
 
 Todo:
 1. Using Advanced Custom Fields (ACF), add additional custom fields to the "jl_job" post type. These fields could include secondary information such as "Reports to" and "Team size".
+
+Endpoints:
+http://localhost/dev/wp-json/wp/v2/jl-jobs
+http://localhost/dev/wp-json/wp/v2/jl-categories
+http://localhost/dev/wp-json/wp/v2/jl-locations
+http://localhost/dev/wp-json/wp/v2/jl-types
+http://localhost/dev/wp-json/wp/v2/jl-experience-levels
--- src/components/JobItem.js
+++ src/components/JobItem.js
@@ -1,6 +1,24 @@
+import { ReactComponent as LocationIcon } from '../icons/location.svg';
+
 function JobItem() {
   return (
-    <li className="jl-job-item">JobItem</li>
+    <li className="jl-job-item">
+      <a className="jl-job-item__link" href="#">
+        <div className="jl-job-item__table">
+          <div className="jl-job-item__row">
+            <div className="jl-job-item__job">
+              <div className="jl-job-item__title">Data Center Hardware Specialist</div>
+              <div className="jl-job-item__subtitle">
+                Job ID: 454233 | Updated on: <span className="jl-job-item__updated">4/21/2023</span>
+              </div>
+            </div>
+            <div className="jl-job-item__category">Hardware Development</div>
+            <div className="jl-job-item__location"><LocationIcon style={{ verticalAlign: 'middle' }} /> <span style={{ verticalAlign: 'middle' }}>Remote</span></div>
+            <div className="jl-job-item__type">Entry Level</div>
+          </div>
+        </div>
+      </a>
+    </li>
   )
 }
 
