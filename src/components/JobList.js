df4f9c01726316347af9e3e7aaf040d0da72e15a
Marios Sofokleous <marios.sofokleous@yandex.com>
2023-05-16 23:07:18 +0300

    Use 'jobList' to render job items with iteration.

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,15 +1,38 @@
+import { useRecoilValue } from 'recoil';
+import { filteredJobsState } from '../recoil-state';
 import JobItem from "./JobItem";
 
 function JobList() {
+  /**
+   * State variables
+   * - Use useRecoilValue() when a component intends to read state without writing to it.
+   */
+  const filteredJobs = useRecoilValue(filteredJobsState);
+
+  const jobItems = filteredJobs.map(job =>
+    <JobItem
+      key={job.id}
+      modifiedGMT={job.modified_gmt}
+      title={job.title}
+      content={job.content}
+      location={job.location}
+      category={job.category}
+      type={job.type}
+      experienceLevel={job.experience_level}
+    />
+  );
+
   return (
-    <ul className="jl-job-list">
-      <JobItem />
-      <JobItem />
-      <JobItem />
-      <JobItem />
-      <JobItem />
-      <JobItem />
-    </ul>
+    <>
+      {filteredJobs.length > 0
+        ?
+          <ul className="jl-job-list">
+            {jobItems}
+          </ul>
+        :
+          <div>No jobs found matching your filtering criteria.</div>
+      }
+    </>
   )
 }
 
