2f23b4bd2ed7bb56661e5f190234bf055800924e
Marios Sofokleous <marios.sofokleous@yandex.com>
2023-05-20 21:35:20 +0300

    Refactor Single component to sanitize and render job title and content.

--- src/routes/Single.js
+++ src/routes/Single.js
@@ -1,8 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { allJobsState } from '../recoil-state';
+import DOMPurify from 'dompurify';
 
 function Single() {
+  /**
+   * State variables
+   * - Use useRecoilValue() when a component intends to read state without writing to it.
+   */
+  const allJobs = useRecoilValue(allJobsState);
 
   // Get the :idDashSlug parameter from the URL.
   const { idDashSlug} = useParams();
@@ -10,17 +16,17 @@ function Single() {
   // Extract the job id.
   const id = parseInt(idDashSlug.split('-').shift());
 
-  /**
-   * State variables
-   * - Use useRecoilValue() when a component intends to read state without writing to it.
-   */
-  const allJobs = useRecoilValue(allJobsState);
-
+  // Find the job 
   const job = allJobs.find((job) => job.id == id);
 
+  // Job details
+  const title = { __html: DOMPurify.sanitize(job.title) };
+  const content = { __html: DOMPurify.sanitize(job.content) };
+
   return (
-    <div>
-      {job.title}
+    <div className="jl-single">
+      <h1 className="jl-single__title" dangerouslySetInnerHTML={ title }></h1>
+      <div className="jl-single__content" dangerouslySetInnerHTML={ content }></div>
     </div>
   )
 }
