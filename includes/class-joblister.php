185987d2d63aa2594bef6d06a9c7b50d94736b4e
Marios Sofokleous <marios.sofokleous@yandex.com>
2023-10-30 20:22:05 +0200

    Setup activation and deactivation hooks

--- includes/class-joblister.php
+++ includes/class-joblister.php
@@ -37,14 +37,14 @@ if (!class_exists('JobLister')) {
     // Initialize classes
     private function initialize_classes()
     {
-      $joblister_dependencies = new Joblister_Dependencies();
-      $joblister_scripts = new Joblister_Scripts();
-      $joblister_styles = new Joblister_Styles();
-      $joblister_shortcodes = new Joblister_Shortcodes();
+      $joblister_dependencies = new JL_Dependencies();
+      $joblister_scripts = new JL_Scripts();
+      $joblister_styles = new JL_Styles();
+      $joblister_shortcodes = new JL_Shortcodes();
       $joblister_custom_post_types = new JL_Custom_Post_Types();
-      $joblister_custom_taxonomies = new Joblister_Custom_Taxonomies();
-      $joblister_rest_api = new Joblister_REST_API();
-      $joblister_meta_boxes = new Joblister_Meta_Boxes();
+      $joblister_custom_taxonomies = new JL_Custom_Taxonomies();
+      $joblister_rest_api = new JL_REST_API();
+      $joblister_meta_boxes = new JL_Meta_Boxes();
       $joblister_admin_columns = new JL_Admin_Columns();
     }
   }
