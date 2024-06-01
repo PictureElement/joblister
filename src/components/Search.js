ccbf926de922e88157da234fbdfec3a1f1d199f7
Marios Sofokleous <marios.sofokleous@yandex.com>
2024-06-01 11:48:31 +0300

    feat: add autoFocus to search field for improved user experience.

--- src/components/Search.js
+++ src/components/Search.js
@@ -41,6 +41,7 @@ function Search() {
         <div className="jbls-visually-hidden">Search for jobs by keyword</div>
         <SearchIcon />
         <input
+          autoFocus
           autoComplete="off"
           value={searchQuery}
           aria-label="Search for jobs by keyword"
